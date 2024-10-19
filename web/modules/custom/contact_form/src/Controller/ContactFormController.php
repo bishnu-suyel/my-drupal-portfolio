543b5c6911775f9701a02d92128657062d5ab8b5
Suyel Bishnu <suyel2007@gmail.com>
2024-10-19 23:23:13 +0300

    Create contact form controller

---
+++ web/modules/custom/contact_form/src/Controller/ContactFormController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\contact_form\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ContactFormController extends ControllerBase
+{
+
+    public function submit(Request $request)
+    {
+        // Get the data from the request
+        $data = json_decode($request->getContent(), true);
+
+        // Validate and sanitize the data
+        $name = isset($data['name']) ? htmlspecialchars($data['name']) : '';
+        $email = isset($data['email']) ? htmlspecialchars($data['email']) : '';
+        $subject = isset($data['subject']) ? htmlspecialchars($data['subject']) : '';
+        $message = isset($data['message']) ? htmlspecialchars($data['message']) : '';
+
+        // Basic validation
+        if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+            return new JsonResponse(['error' => 'All fields are required.'], Response::HTTP_BAD_REQUEST);
+        }
+
+        // Optionally: Save to database or send an email
+        // For example, you could use the MailManager service to send an email
+
+        // Return a success response
+        return new JsonResponse(['status' => 'success', 'message' => 'Your message has been sent.']);
+    }
+}
